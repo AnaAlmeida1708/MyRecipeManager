abce3df242826a094d89deb81ba8444309bcfd47
anaalmeida1708 <anacarolinama@gmail.com>
2020-04-04 11:11:08 -0300

    Projeto criado

---
+++ src/main/java/br/com/myrecipemanager/MyRecipeManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.myrecipemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecipeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/myrecipemanager/MyRecipeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.myrecipemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
